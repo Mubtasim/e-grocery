ee43b2d3ba4e5efeebc5ad4b2649887eb5ebc55d
mubtasim <mubtasim91@gmail.com>
2023-09-21 04:53:15 +0600

    Make product images dynamic

--- scripts/products.js
+++ scripts/products.js
@@ -15,7 +15,7 @@ window.addEventListener("DOMContentLoaded", () => {
       result += `
       <a class="product__item" href="/pages/products/index.html?${PRODUCT_QUERY}=${product.id}">
         <img
-          src="https://chaldn.com/_mpimage/kitchen-appliances?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D29089&q=best&v=1&m=400&webp=1"
+          src=${product.imageUrl}
           class="product__image"
         />
         <div class="product__name">${product.name}</div>
