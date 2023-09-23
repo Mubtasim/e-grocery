834c646b74c2ff8bea8a56c4c10db31eb9f2abb7
mubtasim <mubtasim91@gmail.com>
2023-09-24 00:45:35 +0600

    Remove cart from navigation in checkout page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,4 +3,7 @@ import { getCart, renderCheckoutCartItem } from "./utils.js";
 window.addEventListener("DOMContentLoaded", () => {
   let cart = getCart();
   renderCheckoutCartItem(cart);
+
+  const navRightEl = document.getElementById("nav-right");
+  navRightEl.classList.add("nav__right-disable");
 });
